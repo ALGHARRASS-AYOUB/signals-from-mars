f0c8d2cebeefd1eb48477ab0cdd714817f0fc4a4
ALGHARRASS-AYOUB <ALGHARRASS.ayoub@gmail.com>
2022-11-13 14:42:22 +0100

    v1 commit

--- app/src/main/java/com/example/signalsfrommars/MainActivity.java
+++ app/src/main/java/com/example/signalsfrommars/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.signalsfrommars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText name_edit_text;
+        Button start_button;
+
+        name_edit_text=findViewById(R.id.NameEditText);
+        start_button=findViewById(R.id.startButton  );
+
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=name_edit_text.getText().toString();
+                startStory(name);
+            }
+        });
+
+    }
+
+    private void startStory(String name) {
+        Intent intent=new Intent(getApplicationContext(),StoryAcitivity.class);
+        Resources resources=getResources();
+        String key=resources.getString(R.string.key_name);
+        intent.putExtra(key,name);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/signalsfrommars/StoryAcitivity.java
@@ -0,0 +1,21 @@
+package com.example.signalsfrommars;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+
+public class StoryAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+        Intent from_main_intent=getIntent();
+        String name=from_main_intent.getStringExtra(getString(R.string.key_name));
+        if(name.isEmpty() || name.equals(""))
+            name="guest";
+
+    }
+}
