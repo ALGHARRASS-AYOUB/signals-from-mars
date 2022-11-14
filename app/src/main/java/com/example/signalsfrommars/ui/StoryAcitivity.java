153e767d8ee9257c117462aa07b2ac24e8175531
ALGHARRASS-AYOUB <ALGHARRASS.ayoub@gmail.com>
2022-11-14 14:33:04 +0100

    commit MVP

--- app/src/main/java/com/example/signalsfrommars/ui/StoryAcitivity.java
+++ app/src/main/java/com/example/signalsfrommars/ui/StoryAcitivity.java
@@ -1,14 +1,28 @@
 package com.example.signalsfrommars.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.signalsfrommars.R;
+import com.example.signalsfrommars.model.Page;
+import com.example.signalsfrommars.model.Story;
 
 public class StoryAcitivity extends AppCompatActivity {
+
+    private  Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1_button;
+    private Button choice2_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,16 @@ public class StoryAcitivity extends AppCompatActivity {
         String name=from_main_intent.getStringExtra(getString(R.string.key_name));
         if(name.isEmpty() || name.equals(""))
             name="guest";
+            story=new Story();
+            loadPage(0);
+
+    }
+
+    private void loadPage(int numPage) {
+        Page page=story.getPage(numPage);
+        Drawable image= ContextCompat.getDrawable(this,page.getImageId());
+        storyImageView.setImageDrawable(image);
+//        storyTextView.setText(page.getTextId());
 
     }
 }
